e8b196da498d51774120c0b00273a5bbe84737a9
rolzwy7 <rolzwy7@gmail.com>
2018-08-03 13:59:42 +0200

    added example, readme dependencies reminder

---
+++ examples/generate_report_data.py
@@ -0,0 +1,77 @@
+"""
+    This example shows how to fetch data from mailgun api to generate json file with
+    analytics data for single tag.
+
+
+"""
+
+try:
+    from mgapi.mgapi import Api as MailgunApi
+except:
+    print("Can't find mgapi module.")
+    exit(0)
+
+import pprint as pp
+
+def getReportData(tag):
+    """
+        Generate mailing report data by single tag
+    """
+    # Connect to API
+    api = MailgunApi(config_file="C:\\Users\\Account\Desktop\\config.json")
+    # Return
+    ret = {
+        "tag_info": {},
+        "tag_stats": {},
+        "aggregates": {},
+        "tag_events": {}
+    }
+    # validate, check tag
+    des, ser = api.get_tags(tag=tag)
+    if not des["justify"]["success"]:
+        print(des["justify"]["reason"])
+        return False
+    ret["tag_info"] = des
+
+    # Get aggregates
+    for aggregate in api._AGGREGATES:
+        des, ser = api.get_tag_aggregates(tag=tag, aggregate=aggregate)
+        ret["aggregates"][aggregate] = des
+
+    # Get tag stats
+    for event in api._EVENTS:
+        des, ser = api.get_tag_stats(tag, event, start=api.nowRFC2822(days=-30))
+        ret["tag_stats"][event] = des
+
+    # Get tag events
+    filter_fields = api.ret_events_filter_fields()
+    filter_fields["tags"] = [tag]
+    for event in api._EVENTS:
+        tmp_array = []
+        filter_fields["event"] = event
+        # Get events for tag
+        des, ser = api.get_events(
+            begin=api.nowRFC2822(days= -30),
+            # we can set end parameter a little bit in the future
+            # just in case
+            end=api.nowRFC2822(minutes= 30),
+            filter_fields=filter_fields,
+            limit=290
+        )
+        tmp_array += des["items"]
+        exhausted = False
+        while not exhausted:
+            exhausted, des, ser = api.follow_pagination(des)
+            tmp_array += des["items"]
+        ret["tag_events"][event] = tmp_array
+
+    ret_json = api.serialize_json(ret)
+    return ret, ret_json
+
+## Change tag to your tag
+des, ser = getReportData(tag="MyTag")
+print(ser)
+
+with open("report_test.json", "wb") as source:
+    source.write(ser.encode("utf8"))
+    source.close()
--- mgapi/mgapi.py
+++ mgapi/mgapi.py
@@ -269,6 +269,43 @@ class MGApiUtils(MGApiLogging):
         if return_timestamp:
             return result, now_timestamp
         return result
+    def toRFC2822(_datetime, days=0, hours=0, minutes=0, return_timestamp=False):
+        timedelta_shift = datetime.timedelta(days=days, hours=hours, minutes=minutes)
+        now_datetime = _datetime + timedelta_shift
+        now_timestamp = time.mktime(now_datetime.timetuple())
+        # localtime=True (Sets local timezone)
+        result = utils.formatdate(now_timestamp, localtime=True)
+        # self.print_debug("timedelta_shift:", timedelta_shift)
+        # self.print_debug("now_datetime:", now_datetime)
+        # self.print_debug("now_timestamp:", now_timestamp)
+        # self.print_debug("result:", result)
+        if return_timestamp:
+            return result, now_timestamp
+        return result
+    def ISO8601(iso8601_string):
+        # Date
+        try:
+            date_part = iso8601_string.split("T")[0]
+            year, month, day = date_part.split("-")
+            year, month, day = int(year), int(month), int(day)
+        except Exception as e:
+            print("Exception:", e)
+            return False
+        # Time
+        try:
+            time_part = iso8601_string.split("T")[1].strip("Z")
+            time_part = time_part.split(".")[0] if "." in time_part else time_part
+            hour, minute, second = time_part.split(":")
+            hour, minute, second = int(hour), int(minute), int(second)
+        except Exception as e:
+            print("Exception:", e)
+            hour, minute, second = 0, 0, 0
+        result = datetime.datetime(
+            year=year     , month=month   , day=day,
+            hour=hour     , minute=minute , second=second,
+            microsecond=0 , tzinfo=None
+        )
+        return result
 # Unification of request responses
 class MGApiRequests(MGApiUtils):
 
